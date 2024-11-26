3b01152940650ad4858e853e0043cda23fbc2ab1
djoreally <momspublic@gmail.com>
2024-11-26 00:05:31 -0500

    eblishe3

--- components/ui/TransitionTextComp.tsx
+++ components/ui/TransitionTextComp.tsx
@@ -4,22 +4,28 @@ import React, { useState, useEffect } from 'react';
 import TextTransition, { presets } from 'react-text-transition';
 
 export default function TransitionTextComp() {
-
-  const TEXTS = ['Доступность', 'Надежность', 'Пунктуальность', 'Новые машины'];
+  const TEXTS = ['Accessibility', 'Reliability', 'Punctuality', 'New Cars'];
 
   const [index, setIndex] = useState(0);
 
   useEffect(() => {
     const intervalId = setInterval(
       () => setIndex((index) => index + 1),
-      2000, // every 3 seconds
+      2000 // every 2 seconds
     );
     return () => clearTimeout(intervalId);
   }, []);
-  return (
-            <>
-                <TextTransition springConfig={presets.wobbly} inline={true} className='banner__text-transition' >{TEXTS[index % TEXTS.length]}</TextTransition><p style={{display: "inline", fontSize: '40px'}}>!</p>
-          </>      
 
+  return (
+    <>
+      <TextTransition
+        springConfig={presets.wobbly}
+        inline={true}
+        className='banner__text-transition'
+      >
+        {TEXTS[index % TEXTS.length]}
+      </TextTransition>
+      <p style={{ display: "inline", fontSize: '40px' }}>!</p>
+    </>
   );
 }
